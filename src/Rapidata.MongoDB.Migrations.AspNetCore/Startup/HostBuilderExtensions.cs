b262aceeef4bbbd40642faadc716de655549e2dd
Jorge Paravicini <j.m.paravicini@gmail.com>
2024-10-21 16:01:43 +0200

    feat: use service provider migration resolver

--- src/Rapidata.MongoDB.Migrations.AspNetCore/Startup/HostBuilderExtensions.cs
+++ src/Rapidata.MongoDB.Migrations.AspNetCore/Startup/HostBuilderExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Rapidata.MongoDB.Migrations.AspNetCore.Services;
 using Rapidata.MongoDB.Migrations.Config;
 using Rapidata.MongoDB.Migrations.Core;
 using Rapidata.MongoDB.Migrations.Services;
@@ -22,7 +23,7 @@ public static class HostBuilderExtensions
             {
                 services.AddSingleton<IMigrationService, MigrationService>();
                 services.AddSingleton<MigrationEngine>();
-                services.AddSingleton<IMigrationResolver, MigrationResolver>();
+                services.AddSingleton<IMigrationResolver, ServiceProviderMigrationResolver>();
                 services.AddSingleton(configBuilder.Build(logger));
             });
     }
